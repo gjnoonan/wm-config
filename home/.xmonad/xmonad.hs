e1571604bd3c2cc21b43e642f5bd3cad8ece992a
Gavin-John Noonan <gavinjohn.noonan@gmail.com>
2010-04-20 19:53:59 +0100

    xmonad didn't like the workspace code, so rather than fix it I am just going to have 4 desktops, for now anyways

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,7 +1,7 @@
 import XMonad
  
 main = xmonad defaultConfig
-         { terminal = "urxvtc"
-				 , workspaces = ["1-main","2-web","3-chat" "4:dev"] ++ map show [5..9]
+         { terminal 	= "urxvtc"
+				 , workspaces = ["1:main","2:web","3:chat" "4:dev"]
          }
 
